090b919e598b2b4543ee3e5adcd20a7313ea32e8
koonzinc <koonz.eth@gmail.com>
2022-10-28 13:53:08 -0400

    update firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBs5b7_DmGybJekxuoi8PC3_P5rhoncAcI",
+    authDomain: "whats-app-mern-609f5.firebaseapp.com",
+    projectId: "whats-app-mern-609f5",
+    storageBucket: "whats-app-mern-609f5.appspot.com",
+    messagingSenderId: "717110272153",
+    appId: "1:717110272153:web:c768b82d2f9a49206b5808"
+  };
