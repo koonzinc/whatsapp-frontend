cd80d0a7a7daafb9aefc834e5ddb46627dbc2c43
koonzinc <koonz.eth@gmail.com>
2022-10-29 14:31:43 -0400

    update http error axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Chat from './components/Chat';
 import Sidebar from './components/Sidebar';
+import Pusher from 'pusher-js';
+import axios from './axios';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get('/messages/sync').then((response) => {
+        setMessages(response.data);
+      });
+  }, [])
+
+  useEffect(() => {
+    const pusher = new Pusher('d19ae92716e4ba25ca72', {
+      cluster: 'us2'
+    });
+    // .subscribe('messages') and 'inserted' is found in server.js 
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function (data) {
+      alert(JSON.stringify(data));
+    });
+  }, [])
+
+  console.log(messages)
+
   return (
     <div className="app">
       <div className="app__body">
